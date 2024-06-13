91a1ed6999756b03068064b31f19fd3043f068ae
Ruma303 <matteoh8@hotmail.it>
2024-06-13 17:47:15 +0200

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [],
+    purge: [],
+    darkMode: false,
+    content: ["./Lezioni/*/*.html"],
     theme: {
+        screens: {
+            'xs': '480px',
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+            '2xl': '1536px',
+            },
         extend: {},
+        colors: {}
     },
     plugins: [],
+    variants: [],
+    presets: []
 }
 
